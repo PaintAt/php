d29ebb8846e71d94e66e58d73967882d6fc2a8ef
Kevin <ZetQju@gmail.com>
2020-02-13 12:30:53 +0100

    fix

--- layout/footer.php
+++ layout/footer.php
@@ -27,8 +27,8 @@
         $seconds = gmdate("s", $lastVisitInSec);
 
         echo "<p>";
-        echo "Dnes je " . $datum . ", čas od Vaší poslední návštěvy či aktualizace je $hours hodin $minutes minut $seconds vteřin.";
-        echo "</p>";
+        echo "Dnes je " . $datum . ", čas Vaší poslední návštěvy či aktualizace je $hours hodin $minutes minut $seconds vteřin.";
+        echo "<p>";
         // POČET NÁVŠTĚV
         $pathVisits = "./layout/footerVisits.txt";
         $fileFooter = fopen($pathVisits, "r+");
